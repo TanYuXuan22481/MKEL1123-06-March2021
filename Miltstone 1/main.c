517242e49b8b42c22c1c6b30d4307bf49087702d
TanYuXuan22481 <55542942+TanYuXuan22481@users.noreply.github.com>
2021-04-07 20:02:08 +0800

    Milestone 1 Coding

---
+++ Miltstone 1/main.c
@@ -0,0 +1,47 @@
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+//		method 1
+//		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+//		HAL_Delay(1000);
+//		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+		
+//		method 2
+//		PORTA PIN5 is pointing to LD2 in NUCLEO_F446RE
+	    HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+			HAL_Delay(500); //0.5s
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
